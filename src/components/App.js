ebc8554f2f857ffaee6f1b5c4a069d3f6a802608
solu22 <79486506+solu22@users.noreply.github.com>
2021-03-11 11:04:19 +0200

    Css done and the final card sample is ready..

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,38 @@
 import React from "react";
-import Card from "./Card";
 
-
-
-
-
-
-const App=()=>{
-return(
- <div>
-    <h2>Props with images</h2>
-        
-        <Card 
-        name="Img1" 
-        img="https://picsum.photos/200"
-        email="1@yahoo.com"
-        />
-
-        <Card
-        name="img2"
-        img="https://picsum.photos/200"
-        email="2@yahoo.com"
-        
-         />
-
-  </div>
-)
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">Props with images</h1>
+      <div className="card-details">
+            <div className="upper-details">
+                <h2 className="imgheading">Img1</h2>
+                <img
+                    src="https://picsum.photos/200"
+                    alt="img1"
+                    className="circle-img"
+                />
+            </div>
+            <div className="lower-details">
+            <p className="info">Call: +3581111111</p>
+            <p className="info">Email: img1@yahoo.com</p>
+            </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
+
+//    {/* <Card
+//     name="Img1"
+//     img="https://picsum.photos/200"
+//     email="1@yahoo.com"
+//     />
+
+//      <Card
+//     name="img2"
+//     img="https://picsum.photos/200"
+//     email="2@yahoo.com"
+
+//      /> */}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,17 @@
 const Card = (props) => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="rand1"/>
-      <p>Email: {props.email}</p>
+   <h1 className="heading">Details</h1>
+    <div className="card-details">
+				<div className="upper-details">
+						<h2>{props.name}</h2>
+						<img src={props.img} alt="rand1" />
+				</div>
+
+     <div className="lower-details">
+         <p>Email: {props.email}</p>
+		 </div>
     </div>
-  )
+  );
 };
 
 export default Card;
